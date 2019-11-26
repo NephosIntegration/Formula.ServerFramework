1d03eb54cdd329a1de1aceafcefd365264f4e5cf
Joshua Williams <joshua.p.williams@gmail.com>
2019-11-25 23:28:53 -0600

    Started authentication

--- templates/Formula.MyApi/Startup.cs
+++ templates/Formula.MyApi/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Formula.SimpleRepo;
+using Formula.SimpleApi.Auth;
 
 namespace Formula.MyApi
 {
@@ -28,6 +29,7 @@ namespace Formula.MyApi
         {
             services.AddControllers();
             services.AddRepositories();
+            services.AddAuth();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
